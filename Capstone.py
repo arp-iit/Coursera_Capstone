36d0553fc7d81a5219dbac99cedcbf9121139e39
ARPIT BHARDWAJ <68179849+arp-iit@users.noreply.github.com>
2020-07-12 13:35:38 +0530

    Add files via upload

---
+++ Capstone.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be used specifically for the purpose of completing my Coursera Capstone Project.
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
